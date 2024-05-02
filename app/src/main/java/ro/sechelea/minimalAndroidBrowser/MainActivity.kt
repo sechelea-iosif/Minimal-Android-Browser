37d7337a3316e4ec4a91ce572601d80871986dd7
sechelea.iosif <sechelea.iosif@gmail.com>
2024-05-02 15:30:14 +0300

    Restructure Application Theme and Entry Point

--- app/src/main/java/ro/sechelea/minimalAndroidBrowser/MainActivity.kt
+++ app/src/main/java/ro/sechelea/minimalAndroidBrowser/MainActivity.kt
@@ -3,44 +3,44 @@ package ro.sechelea.minimalAndroidBrowser
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ro.sechelea.minimalAndroidBrowser.ui.Background
 import ro.sechelea.minimalAndroidBrowser.ui.theme.MinimalAndroidBrowserTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MinimalAndroidBrowserTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    App()
+}
+
+@Composable
+private fun App() {
+    MinimalAndroidBrowserTheme().Show {
+        Background {
+            Greeting()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    modifier: Modifier = Modifier,
+    name: String = "World",
+) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MinimalAndroidBrowserTheme {
-        Greeting("Android")
-    }
 }
